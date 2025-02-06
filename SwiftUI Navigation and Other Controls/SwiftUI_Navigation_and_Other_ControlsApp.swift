4664570eb04e8b123fe951cfdfcccd00f40fe276
kylekaufman <ktk33@case.edu>
2025-02-06 18:17:10 -0500

    Initial Commit

---
+++ SwiftUI Navigation and Other Controls/SwiftUI_Navigation_and_Other_ControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Navigation_and_Other_ControlsApp.swift
+//  SwiftUI Navigation and Other Controls
+//
+//  Created by Kyle Kaufman on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Navigation_and_Other_ControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
